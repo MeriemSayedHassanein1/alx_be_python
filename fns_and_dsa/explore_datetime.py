cb7d12936c6afc379fa65a53ea00fdf5432c6242
MeriemSayedHassanein1 <elayki.bijouterie.manager@gmail.com>
2025-10-01 12:34:37 +0100

    mer

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,23 +1,18 @@
 import datetime
 
-nowtime=datetime.datetime.now()
-print(nowtime)
-
 def display_current_datetime():
-    current_date=datetime.datetime.now()
-    print("Current date and time:", current_date.strftime("%Y-%m-%d %H:%M:%S"))
-
+    current_date = datetime.datetime.now()
+    return current_date.strftime("%Y-%m-%d %H:%M:%S")
 
 def calculate_future_date(number_of_days):
-    current_date=datetime.datetime.now()
-    future_date=current_date+datetime.timedelta(days=number_of_days)
-    print(("Future date is : ",future_date.strftime("%Y-%m-%d")))
-
+    current_date = datetime.datetime.now()
+    future_date = current_date + datetime.timedelta(days=number_of_days)
+    return future_date.strftime("%Y-%m-%d")
 
 def main():
-    display_current_datetime()
+    print("Current date and time:", display_current_datetime())
     days = int(input("Enter number of days to add: "))
-    calculate_future_date(days)
+    print("Future date is:", calculate_future_date(days))
 
 if __name__ == "__main__":
     main()
